b68111edb27243d7e93929f2ecd21b35b84a803d
M-Farag <mina.farag@icloud.com>
2023-04-12 01:47:15 +0200

    ADD A proof of concept for the service logger
    ADD Accepting argumnets
    ADD Opening a file for appending
    ADD Writing to a file'

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::env;
+use std::fs;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    // Todo
+    // [x] accept arguments
+    // [x] define file_path & a message
+    // [x] Open a file for appending
+    // [x] Write the message to the file
+    // [x] check the results
+
+    let args:Vec<String> = env::args().collect();
+
+    let file_path = &args[1];
+    let message = &args[2];
+
+    let mut file_1 = fs::OpenOptions::new()
+    .append(true)
+    .create(true)
+    .open(file_path).unwrap();
+
+    writeln!(file_1,"{}",message);
+
+
+    
 }

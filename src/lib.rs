b9c4531820f2f9144e5913ff376e50aca84cf103
M-Farag <mina.farag@icloud.com>
2023-04-14 00:10:07 +0200

    ADD A lib module
    UPD Migrate the arguments functionality to thelib module
    UPD Migrate the file handler to the lib module

---
+++ src/lib.rs
@@ -0,0 +1,42 @@
+use std::env;
+use std::error::Error;
+use std::fs;
+use std::io::Write;
+
+#[derive(Debug)]
+pub struct Config {
+    file_path:String,
+    message:String,
+}
+
+impl Config {
+    pub fn new() -> Result<Config, &'static str> {
+        let args:Vec<String> = env::args().collect();
+
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let file_path = args[1].clone();
+        let message = args[2].clone();
+        Ok(Config {file_path,message})
+    }
+
+    pub fn file_path(&self) -> &String {
+        &self.file_path
+    }
+
+    pub fn message(&self) -> &String {
+        &self.message
+    }
+
+    pub fn write_to_file(&self) -> Result<(),Box<dyn Error>>
+     {
+        let mut file_1 = fs::OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(self.file_path())?;
+
+        writeln!(file_1,"{}",self.message())?;
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,27 @@
-use std::env;
 use std::fs;
 use std::io::Write;
+use demos_service_logger::*;
 
 fn main() {
     // Todo
-    // [x] accept arguments
-    // [x] define file_path & a message
-    // [x] Open a file for appending
-    // [x] Write the message to the file
-    // [x] check the results
+    // [x] Creating a lib module
+    // [x] Migrate the env args functionality to the lib module
+    // [x] Make sure that the lib module is able to handle the arguments
+    // [] Migrate the file handler to the lib module
+    // [] Make sure that the lib module is able to handle the file
+    // [] Error handling
+    // [] Testing that everything works
 
-    let args:Vec<String> = env::args().collect();
+    
 
-    let file_path = &args[1];
-    let message = &args[2];
+    let config = Config::new().unwrap_or_else(
+        |err| {
+            println!("Err: {}",err);
+            std::process::exit(1);
+        }
+    );
 
-    let mut file_1 = fs::OpenOptions::new()
-    .append(true)
-    .create(true)
-    .open(file_path).unwrap();
-
-    writeln!(file_1,"{}",message);
+    config.write_to_file().unwrap()
 
 
     
